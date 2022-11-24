ade9e4b027df0a85f121cfb8146e8690f9092bac
Raisul999 <raisulislamarnob0@gmail.com>
2022-11-25 00:34:55 +0600

    file structure change

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,13 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 // Serve frontend 
 
-if(process.env.NODE_ENV === 'production'){
-   app.use(express.static(path.join(__dirname, '../frontend/build')))
+// if(process.env.NODE_ENV === 'production'){
+//    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-   app.get('*', (req,res)=>res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html')))
-}else{
-    app.get('/', (req, res)=>res.send('Please set to production'))
-}
+//    app.get('*', (req,res)=>res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html')))
+// }else{
+//     app.get('/', (req, res)=>res.send('Please set to production'))
+// }
 
 app.use(errorHandler)
 app.listen(port, ()=>console.log(`Server started on port ${port}`));
