fc53e6cf05383b2ca044c83b4e5a8fcf513f50bd
Raisul999 <raisulislamarnob0@gmail.com>
2022-11-25 11:07:16 +0600

    cors handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const colors = require('colors');
 const connectDB = require('./config/db');
 const {errorHandler} = require('./middleware/errorMiddleware');
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 connectDB()
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
